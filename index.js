9ca013461fac71c2ae063c2ad045a0a3ea81f909
JOB KIPTOO <114221274+KIPTOO-JOB@users.noreply.github.com>
2024-06-26 21:52:10 +0000

    main

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 2 
+const num2 = 31
+
+const multiply = num1 * num2
+
+let num3 = 1; 
+let num4 = 0;
+
+let mod = (num4 * 3) + 4
+
+let random = Math.floor(+1)
+
+let max = Math.max(5,10,20)
